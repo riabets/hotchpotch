f07848383853c952730bfc935af9adc7262d5a2e
marina.riabets <m.riabets@electroco3000.de>
2018-04-20 12:15:18 +0300

    because js was in wrong place

--- canvas.js
+++ canvas.js
@@ -1,5 +1,5 @@
-const c = document.getElementById('canvas');
-const d = c.getContext("2d");
+var canvas = document.querySelector("canvas");
+var d = canvas.getContext("2d");
 d.beginPath();
-d.arc(95, 50, 40, 0, 2 * Math.PI);
+d.arc(185, 50, 40, 0, 2 * Math.PI);
 d.stroke();
