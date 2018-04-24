92097dfea8486059f9a81cf00af7f32f5769b9cd
marina.riabets <m.riabets@electroco3000.de>
2018-04-24 15:51:55 +0300

    bigger canvas size

--- canvas.js
+++ canvas.js
@@ -1,5 +1,8 @@
 var canvas = document.querySelector("canvas");
 var d = canvas.getContext("2d");
 d.beginPath();
-d.arc(185, 50, 40, 0, 2 * Math.PI);
+d.arc(190, 50, 40, 0, 2 * Math.PI);
 d.stroke();
+
+d.fillStyle = "#8BC34A";
+d.fillRect(90, 40, 60, 20);
