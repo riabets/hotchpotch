e680c3c3e8ff323fd39e42728aaca604ed3b9459
marina.riabets <m.riabets@electroco3000.de>
2018-05-02 13:22:55 +0300

    more to curves

--- canvas.js
+++ canvas.js
@@ -1,8 +1,24 @@
 var canvas = document.querySelector("canvas");
 var d = canvas.getContext("2d");
+
+// Circle
 d.beginPath();
-d.arc(190, 50, 40, 0, 2 * Math.PI);
+d.arc(190, 60, 60, 0, 2 * Math.PI);
 d.stroke();
 
+// Rectangle
 d.fillStyle = "#8BC34A";
 d.fillRect(90, 40, 60, 20);
+
+// ArcTo
+d.beginPath();
+d.moveTo(450, 20);
+d.arcTo(150, 100, 50, 20, 30);
+d.lineTo(50, 20);
+d.stroke();
+
+// BezierCurve
+d.beginPath();
+d.moveTo(50,20);
+d.bezierCurveTo(230, 30, 150, 60, 50, 100);
+d.stroke();
