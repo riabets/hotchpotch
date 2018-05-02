933d5407f3efe535cdbb37d8d29323fe80e5459b
marina.riabets <m.riabets@electroco3000.de>
2018-05-02 14:54:23 +0300

    gradient

--- canvas.js
+++ canvas.js
@@ -1,6 +1,6 @@
 var canvas = document.querySelector("canvas");
 var d = canvas.getContext("2d");
-
+var gradient = d.createLinearGradient(30, 30, 300, 300);
 // Circle
 d.beginPath();
 d.arc(190, 60, 60, 0, 2 * Math.PI);
@@ -15,10 +15,23 @@ d.beginPath();
 d.moveTo(450, 20);
 d.arcTo(150, 100, 50, 20, 30);
 d.lineTo(50, 20);
+d.closePath();
 d.stroke();
 
 // BezierCurve
 d.beginPath();
 d.moveTo(50,20);
 d.bezierCurveTo(230, 30, 150, 60, 50, 100);
+d.closePath();
 d.stroke();
+
+// Linear Gradient
+gradient.addColorStop(0, 'green');
+gradient.addColorStop(1, 'yellow');
+d.fillStyle = gradient;
+d.fillRect(30, 30, 300, 300);
+
+// Clearig block
+d.clearRect(150, 50, 50, 50);
+// Clearing full canvas
+// d.clearRect(0, 0, canvas.width, canvas.height);
